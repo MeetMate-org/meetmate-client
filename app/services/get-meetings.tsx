141194402277f217f63f1cbb80426287676e131a
yuriy-kulakovskyi <yurakulak42@gmail.com>
2025-04-23 07:53:15 +0300

    feat: function to get meetings that user is attending

--- app/services/get-meetings.tsx
+++ app/services/get-meetings.tsx
@@ -12,3 +12,16 @@ export const getMeetingsByUserId = async (userId: string, token: string) => {
 
   return res.data;
 }
+
+export const getAttendingMeetings = async (userId: string, token: string) => {
+  const res = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/meetings/user/attending/${userId}`,
+    {
+      headers: {
+        "x-access-token": token,
+      },
+    }
+  );
+
+  return res.data;
+}
