be334427d71ceadf4df4d7849aa449614b67631a
yuriy-kulakovskyi <yurakulak42@gmail.com>
2025-05-05 19:06:07 +0300

    removed useless useEffect

--- app/ui/components/voting-meeting-card.tsx.tsx
+++ app/ui/components/voting-meeting-card.tsx.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { IconUserMeeting } from "../svg/icon-user-meeting";
 import { IconDeadline } from "../svg/icon-deadline";
 import { IconTeam } from "../svg/icon-team";
